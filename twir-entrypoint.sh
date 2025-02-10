bd015834ec9279985b4ea45f0e9f038f202a4767
Satont <satontworldwide@gmail.com>
2025-02-10 21:00:02 +0300

    chore: update db addr

--- twir-entrypoint.sh
+++ twir-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export DB=postgresql
+export DB=temporal-postgres
 export DB_PORT=5432
 export DYNAMIC_CONFIG_FILE_PATH=config/dynamicconfig/development-sql.yaml
 export POSTGRES_SEEDS=postgres
 export POSTGRES_DB=temporal
-export POSTGRES_PWD=$(egrep  -v '^#' /run/secrets/twir_postgres_password)
-export POSTGRES_USER=$(egrep  -v '^#' /run/secrets/twir_postgres_user)
+export POSTGRES_PWD=temporal
+export POSTGRES_USER=temporal
 
 source /etc/temporal/entrypoint.sh
