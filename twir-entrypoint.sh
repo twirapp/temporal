6cb2fea71da0d04d36552d0d352c4cb77c626b22
Satont <satontworldwide@gmail.com>
2025-02-10 21:12:11 +0300

    fix envs

--- twir-entrypoint.sh
+++ twir-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export DB=temporal-postgres
+export DB=postgres
 export DB_PORT=5432
 export DYNAMIC_CONFIG_FILE_PATH=config/dynamicconfig/development-sql.yaml
-export POSTGRES_SEEDS=postgres
+export POSTGRES_SEEDS=temporal-postgres
 export POSTGRES_DB=temporal
 export POSTGRES_PWD=temporal
 export POSTGRES_USER=temporal
